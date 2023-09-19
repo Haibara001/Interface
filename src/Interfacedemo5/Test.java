fe7177ec96725126c423fa2588e502936bd76e37
Haibara001 <118810764+Haibara001@users.noreply.github.com>
2023-09-19 16:44:10 +0800

    Add files via upload

---
+++ src/Interfacedemo5/Test.java
@@ -0,0 +1,13 @@
+package Interfacedemo4;
+
+public class Test {
+    public static void main(String[] args) {
+        /* 接口中私有方法的定义格式：
+         格式1：private 返回值类型 方法名(参数列表）{ }
+         示例1：private void show() {  }
+
+         格式2：private static 返回值类型 方法名（参数列表） { }
+         示例2：private static void method() {  }
+         */
+    }
+}
